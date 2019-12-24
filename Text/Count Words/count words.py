ef77f6cb4c24404c270be50e1a327d10dd669ce9
Vinicius-PR <58795242+Vinicius-PR@users.noreply.github.com>
2019-12-24 16:03:18 -0300

    Count Words

--- Text/Count Words/count words.py
+++ Text/Count Words/count words.py
@@ -1,20 +1,18 @@
 """
 This program counts the number of words in a string. Also display the result for each word and the total.
 """
-import string
+import string as str
 
 
 def count_words(str_func):
     my_dic = {}
     # creating the dictionary:
-    for letter in string.ascii_lowercase:
+    for letter in str_func.split():
         my_dic[letter] = 0
 
     # Counting the words:
-    for s in str_func:
-        for letter in string.ascii_lowercase:
-            if s.lower() == letter:
-                my_dic[letter] += 1
+    for word in str_func.split():
+        my_dic[word] += 1
     return my_dic
 
 
@@ -23,4 +21,4 @@ if __name__ == '__main__':
     words_dic = count_words(str_input)
     print("The number of words:")
     print(words_dic)
-    print("The total numbers of words are: {}".format(sum(words_dic.values())))
+    print("The total number of words is: {}".format(sum(words_dic.values())))
