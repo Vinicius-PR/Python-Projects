a476fdaa742e056222bab95b1efbdfbc2ed4b321
Ajay <118124659+AjayCodes7@users.noreply.github.com>
2024-01-09 21:57:36 +0530

    Update Reverse.py

--- Text/Reverse String/Reverse.py
+++ Text/Reverse String/Reverse.py
@@ -4,5 +4,4 @@ This program just reverse a string and print it.
 
 if __name__ == '__main__':
     string_input = input("Enter a phrase, word or anything: ")
-    reversed_string = string_input[::-1]
-    print("The reversed input is: {}".format(reversed_string))
+    print(f"The reversed input is: {string_input[::-1]}")
